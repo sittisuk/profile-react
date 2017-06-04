8003c3ef91c573102c19db9e8ed37bb60bf06f92
Sittisuk <beerxp1@gmail.com>
2017-06-04 16:15:17 +0700

    edit file app.js little

--- src/App.js
+++ src/App.js
@@ -3,16 +3,30 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    imageURL: ""
+  }
+  componentDidMount() {
+    fetch('http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=american+psycho')
+    .then(res => res.json())
+    .then(json => json.data.image_url)
+    .then((imageURL) => {
+      this.setState({
+        imageURL: imageURL
+      })
+    })
+  }
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to Sittisuk</h2>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <img src={this.state.imageURL} alt="git file"/>
       </div>
     );
   }
